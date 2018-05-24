ecaeaf810750df73cfaba697355e7219677673a4
marf <marvin@proudinnovations.nl>
2018-05-24 11:17:57 +0200

    Added basic frontend.
    Signed-off-by: marf <marvin@proudinnovations.nl>

---
+++ database/migrations/2018_05_24_074727_create_accounts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url');
+            $table->string('username');
+            $table->string('password');
+            $table->string('WriteGroup');
+            $table->string('ReadGroup');
+            $table->string('Tagid');
+            $table->longtext('Description');
+            $table->float('ChangeQueue');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
